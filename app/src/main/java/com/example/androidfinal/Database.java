f300e5be41e56f9c7c6ed9291e447e21c1a8099d
Amanda Gayer <72535138+najiblitz@users.noreply.github.com>
2021-04-08 20:11:48 -0400

    Issue #4 Budget Fragment- * Add all database content for budget tracker * Add Budget Pojo * Update stling and layout

--- app/src/main/java/com/example/androidfinal/Database.java
+++ app/src/main/java/com/example/androidfinal/Database.java
@@ -8,6 +8,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.androidfinal.Pojo.Budget;
 import com.example.androidfinal.Pojo.Money;
 import com.example.androidfinal.Pojo.Saving;
 
@@ -18,6 +19,13 @@ public class Database extends SQLiteOpenHelper {
     public static final int DATABASE_VERSION = 1;
 
     public static final String DATABASE_NAME = "moneyscentsdb";
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_SAVING_TABLE);
+        db.execSQL(CREATE_MONEY_TABLE);
+        db.execSQL(CREATE_BUDGET_TABLE);
+    }
 
     /*
 
@@ -40,12 +48,6 @@ public class Database extends SQLiteOpenHelper {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_SAVING_TABLE);
-        db.execSQL(CREATE_MONEY_TABLE);
-    }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -146,5 +148,193 @@ public class Database extends SQLiteOpenHelper {
         db.close();
         return money;
     }
+
+
+
+
+
+
+
+
+    /*
+
+    BUDGET TRACKER
+
+     */
+
+
+
+    public static final String TABLE_BUDGET = "budget";
+
+    public static final String COLUMN_SALARY = "salary";
+    public static final String COLUMN_HOME = "home";
+    public static final String COLUMN_ELECTRIC = "electric";
+    public static final String COLUMN_OIL = "oil";
+    public static final String COLUMN_TV = "tv";
+    public static final String COLUMN_PHONE = "phone";
+    public static final String COLUMN_HOME_REPAIRS = "home_repairs";
+    public static final String COLUMN_HOME_OTHER = "home_other";
+    public static final String COLUMN_VEHICLE = "vehicle";
+    public static final String COLUMN_INSURANCE = "insurance";
+    public static final String COLUMN_GAS = "gas";
+    public static final String COLUMN_CAR_REPAIRS = "car_repairs";
+    public static final String COLUMN_CAR_OTHER = "car_other";
+    public static final String COLUMN_DINING = "dining";
+    public static final String COLUMN_GROCERIES = "groceries";
+    public static final String COLUMN_HEALTH = "health";
+    public static final String COLUMN_PERSONAL = "personal";
+    public static final String COLUMN_ACTIVITIES = "activities";
+    public static final String COLUMN_SHOWS = "shows";
+    public static final String COLUMN_DAILY_OTHER = "daily_other";
+
+    public static final String CREATE_BUDGET_TABLE = "CREATE TABLE " + TABLE_BUDGET + "(" + COLUMN_ID + " INTEGER PRIMARY KEY, " +
+            COLUMN_SALARY + " DOUBLE, " +
+            COLUMN_HOME + " DOUBLE, " +
+            COLUMN_ELECTRIC + " DOUBLE, " +
+            COLUMN_OIL + " DOUBLE, " +
+            COLUMN_TV + " DOUBLE, " +
+            COLUMN_PHONE + " DOUBLE, " +
+            COLUMN_HOME_REPAIRS + " DOUBLE, " +
+            COLUMN_HOME_OTHER + " DOUBLE, " +
+            COLUMN_VEHICLE + " DOUBLE, " +
+            COLUMN_INSURANCE + " DOUBLE, " +
+            COLUMN_GAS + " DOUBLE, " +
+            COLUMN_CAR_REPAIRS + " DOUBLE, " +
+            COLUMN_CAR_OTHER + " DOUBLE, " +
+            COLUMN_DINING + " DOUBLE, " +
+            COLUMN_GROCERIES + " DOUBLE, " +
+            COLUMN_HEALTH + " DOUBLE, " +
+            COLUMN_PERSONAL + " DOUBLE, " +
+            COLUMN_ACTIVITIES + " DOUBLE, " +
+            COLUMN_SHOWS + " DOUBLE, " +
+            COLUMN_DAILY_OTHER + " DOUBLE)";
+
+
+    public void addBudget(Budget budget) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_SALARY, budget.getSalary());
+        values.put(COLUMN_HOME, budget.getHome());
+        values.put(COLUMN_ELECTRIC, budget.getElectric());
+        values.put(COLUMN_OIL, budget.getOil());
+        values.put(COLUMN_TV, budget.getTv());
+        values.put(COLUMN_PHONE, budget.getPhone());
+        values.put(COLUMN_HOME_REPAIRS, budget.getHomeRepairs());
+        values.put(COLUMN_HOME_OTHER, budget.getHomeOther());
+        values.put(COLUMN_VEHICLE, budget.getVehicle());
+        values.put(COLUMN_INSURANCE, budget.getInsurance());
+        values.put(COLUMN_GAS, budget.getGas());
+        values.put(COLUMN_CAR_REPAIRS, budget.getCarRepairs());
+        values.put(COLUMN_CAR_OTHER, budget.getCarOther());
+        values.put(COLUMN_DINING, budget.getDining());
+        values.put(COLUMN_GROCERIES, budget.getGroceries());
+        values.put(COLUMN_HEALTH, budget.getHealth());
+        values.put(COLUMN_PERSONAL, budget.getPersonal());
+        values.put(COLUMN_ACTIVITIES, budget.getActivities());
+        values.put(COLUMN_SHOWS, budget.getShows());
+        values.put(COLUMN_DAILY_OTHER, budget.getDailyOther());
+
+        db.insert(TABLE_BUDGET, null, values);
+        db.close();
+    }
+
+    public Budget getBudget(int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Budget budget = null;
+        Cursor cursor = db.query(TABLE_BUDGET, new String[]{COLUMN_ID, COLUMN_SALARY, COLUMN_HOME,}, COLUMN_ID + "= ?", new String[]{String.valueOf(id)}, null, null, null);
+        if (cursor.moveToFirst()) {
+            budget = new Budget(cursor.getInt(0),
+                    cursor.getDouble(1),
+                    cursor.getDouble(2),
+                    cursor.getDouble(3),
+                    cursor.getDouble(4),
+                    cursor.getDouble(5),
+                    cursor.getDouble(6),
+                    cursor.getDouble(7),
+                    cursor.getDouble(8),
+                    cursor.getDouble(9),
+                    cursor.getDouble(10),
+                    cursor.getDouble(11),
+                    cursor.getDouble(12),
+                    cursor.getDouble(13),
+                    cursor.getDouble(14),
+                    cursor.getDouble(15),
+                    cursor.getDouble(16),
+                    cursor.getDouble(17),
+                    cursor.getDouble(18),
+                    cursor.getDouble(19),
+                    cursor.getDouble(20)
+            );
+
+        }
+        db.close();
+        return budget;
+    }
+
+    public ArrayList<Budget> getAllBudgets() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT *FROM " + TABLE_BUDGET, null);
+        ArrayList<Budget> budgets = new ArrayList<>();
+        while (cursor.moveToNext()) {
+            budgets.add(new Budget(
+                    cursor.getInt(0),
+                    cursor.getDouble(1),
+                    cursor.getDouble(2),
+                    cursor.getDouble(3),
+                    cursor.getDouble(4),
+                    cursor.getDouble(5),
+                    cursor.getDouble(6),
+                    cursor.getDouble(7),
+                    cursor.getDouble(8),
+                    cursor.getDouble(9),
+                    cursor.getDouble(10),
+                    cursor.getDouble(11),
+                    cursor.getDouble(12),
+                    cursor.getDouble(13),
+                    cursor.getDouble(14),
+                    cursor.getDouble(15),
+                    cursor.getDouble(16),
+                    cursor.getDouble(17),
+                    cursor.getDouble(18),
+                    cursor.getDouble(19),
+                    cursor.getDouble(20)
+            ));
+        }
+        db.close();
+        return budgets;
+
+    }
+
+    public int updateBudget(Budget budget) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_SALARY, budget.getSalary());
+        values.put(COLUMN_HOME, budget.getHome());
+        values.put(COLUMN_ELECTRIC, budget.getElectric());
+        values.put(COLUMN_OIL, budget.getOil());
+        values.put(COLUMN_TV, budget.getTv());
+        values.put(COLUMN_PHONE, budget.getPhone());
+        values.put(COLUMN_HOME_REPAIRS, budget.getHomeRepairs());
+        values.put(COLUMN_HOME_OTHER, budget.getHomeOther());
+        values.put(COLUMN_VEHICLE, budget.getVehicle());
+        values.put(COLUMN_INSURANCE, budget.getInsurance());
+        values.put(COLUMN_GAS, budget.getGas());
+        values.put(COLUMN_CAR_REPAIRS, budget.getCarRepairs());
+        values.put(COLUMN_CAR_OTHER, budget.getCarOther());
+        values.put(COLUMN_DINING, budget.getDining());
+        values.put(COLUMN_GROCERIES, budget.getGroceries());
+        values.put(COLUMN_HEALTH, budget.getHealth());
+        values.put(COLUMN_PERSONAL, budget.getPersonal());
+        values.put(COLUMN_ACTIVITIES, budget.getActivities());
+        values.put(COLUMN_SHOWS, budget.getShows());
+        values.put(COLUMN_DAILY_OTHER, budget.getDailyOther());
+
+        return db.update(TABLE_BUDGET, values, COLUMN_ID + "=?", new String[]{String.valueOf(budget.getId())});
+
+}
+
+
+
+
 
 }
--- app/src/main/java/com/example/androidfinal/Pojo/Budget.java
+++ app/src/main/java/com/example/androidfinal/Pojo/Budget.java
@@ -6,18 +6,140 @@ import android.os.Parcelable;
 public class Budget implements Parcelable {
 
     private int id;
+    // salary
+
+    private double salary;
+
+    // Home
+
     private double home;
+    private double electric;
+    private double oil;
+    private double tv;
+    private double phone;
+    private double homeRepairs;
+    private double homeOther;
+
+    // Vehicle
+
+    private double vehicle;
+    private double insurance;
+    private double gas;
+    private double carRepairs;
+    private double carOther;
+
+    // Daily
+
+    private double dining;
+    private double groceries;
+    private double health;
+    private double personal;
+    private double activities;
+    private double shows;
+    private double dailyOther;
+
+    public Budget(double salary, double home, double electric, double oil, double tv, double phone, double homeRepairs, double homeOther, double vehicle, double insurance, double gas, double carRepairs, double carOther, double dining, double groceries, double health, double personal, double activities, double shows, double dailyOther) {
+        this.salary = salary;
+        this.home = home;
+        this.electric = electric;
+        this.oil = oil;
+        this.tv = tv;
+        this.phone = phone;
+        this.homeRepairs = homeRepairs;
+        this.homeOther = homeOther;
+        this.vehicle = vehicle;
+        this.insurance = insurance;
+        this.gas = gas;
+        this.carRepairs = carRepairs;
+        this.carOther = carOther;
+        this.dining = dining;
+        this.groceries = groceries;
+        this.health = health;
+        this.personal = personal;
+        this.activities = activities;
+        this.shows = shows;
+        this.dailyOther = dailyOther;
+    }
+    public Budget(int id, double salary, double home, double electric, double oil, double tv, double phone, double homeRepairs, double homeOther, double vehicle, double insurance, double gas, double carRepairs, double carOther, double dining, double groceries, double health, double personal, double activities, double shows, double dailyOther) {
+        this.id = id;
+        this.salary = salary;
+        this.home = home;
+        this.electric = electric;
+        this.oil = oil;
+        this.tv = tv;
+        this.phone = phone;
+        this.homeRepairs = homeRepairs;
+        this.homeOther = homeOther;
+        this.vehicle = vehicle;
+        this.insurance = insurance;
+        this.gas = gas;
+        this.carRepairs = carRepairs;
+        this.carOther = carOther;
+        this.dining = dining;
+        this.groceries = groceries;
+        this.health = health;
+        this.personal = personal;
+        this.activities = activities;
+        this.shows = shows;
+        this.dailyOther = dailyOther;
+    }
 
     protected Budget(Parcel in) {
+        id = in.readInt();
+        salary = in.readDouble();
+        home = in.readDouble();
+        electric = in.readDouble();
+        oil = in.readDouble();
+        tv = in.readDouble();
+        phone = in.readDouble();
+        homeRepairs = in.readDouble();
+        homeOther = in.readDouble();
+        vehicle = in.readDouble();
+        insurance = in.readDouble();
+        gas = in.readDouble();
+        carRepairs = in.readDouble();
+        carOther = in.readDouble();
+        dining = in.readDouble();
+        groceries = in.readDouble();
+        health = in.readDouble();
+        personal = in.readDouble();
+        activities = in.readDouble();
+        shows = in.readDouble();
+        dailyOther = in.readDouble();
+    }
+
+    public Budget() {
+
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeDouble(salary);
+        dest.writeDouble(home);
+        dest.writeDouble(electric);
+        dest.writeDouble(oil);
+        dest.writeDouble(tv);
+        dest.writeDouble(phone);
+        dest.writeDouble(homeRepairs);
+        dest.writeDouble(homeOther);
+        dest.writeDouble(vehicle);
+        dest.writeDouble(insurance);
+        dest.writeDouble(gas);
+        dest.writeDouble(carRepairs);
+        dest.writeDouble(carOther);
+        dest.writeDouble(dining);
+        dest.writeDouble(groceries);
+        dest.writeDouble(health);
+        dest.writeDouble(personal);
+        dest.writeDouble(activities);
+        dest.writeDouble(shows);
+        dest.writeDouble(dailyOther);
     }
 
     @Override
     public int describeContents() {
         return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
     }
 
     public static final Creator<Budget> CREATOR = new Creator<Budget>() {
@@ -31,6 +153,181 @@ public class Budget implements Parcelable {
             return new Budget[size];
         }
     };
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public double getHome() {
+        return home;
+    }
+
+    public void setHome(double home) {
+        this.home = home;
+    }
+
+    public double getElectric() {
+        return electric;
+    }
+
+    public void setElectric(double electric) {
+        this.electric = electric;
+    }
+
+    public double getOil() {
+        return oil;
+    }
+
+    public void setOil(double oil) {
+        this.oil = oil;
+    }
+
+    public double getTv() {
+        return tv;
+    }
+
+    public void setTv(double tv) {
+        this.tv = tv;
+    }
+
+    public double getPhone() {
+        return phone;
+    }
+
+    public void setPhone(double phone) {
+        this.phone = phone;
+    }
+
+    public double getHomeRepairs() {
+        return homeRepairs;
+    }
+
+    public void setHomeRepairs(double homeRepairs) {
+        this.homeRepairs = homeRepairs;
+    }
+
+    public double getHomeOther() {
+        return homeOther;
+    }
+
+    public void setHomeOther(double homeOther) {
+        this.homeOther = homeOther;
+    }
+
+    public double getVehicle() {
+        return vehicle;
+    }
+
+    public void setVehicle(double vehicle) {
+        this.vehicle = vehicle;
+    }
+
+    public double getInsurance() {
+        return insurance;
+    }
+
+    public void setInsurance(double insurance) {
+        this.insurance = insurance;
+    }
+
+    public double getGas() {
+        return gas;
+    }
+
+    public void setGas(double gas) {
+        this.gas = gas;
+    }
+
+    public double getCarRepairs() {
+        return carRepairs;
+    }
+
+    public void setCarRepairs(double carRepairs) {
+        this.carRepairs = carRepairs;
+    }
+
+    public double getCarOther() {
+        return carOther;
+    }
+
+    public void setCarOther(double carOther) {
+        this.carOther = carOther;
+    }
+
+    public double getDining() {
+        return dining;
+    }
+
+    public void setDining(double dining) {
+        this.dining = dining;
+    }
+
+    public double getGroceries() {
+        return groceries;
+    }
+
+    public void setGroceries(double groceries) {
+        this.groceries = groceries;
+    }
+
+    public double getHealth() {
+        return health;
+    }
+
+    public void setHealth(double health) {
+        this.health = health;
+    }
+
+    public double getPersonal() {
+        return personal;
+    }
+
+    public void setPersonal(double personal) {
+        this.personal = personal;
+    }
+
+    public double getActivities() {
+        return activities;
+    }
+
+    public void setActivities(double activities) {
+        this.activities = activities;
+    }
+
+    public double getShows() {
+        return shows;
+    }
+
+    public void setShows(double shows) {
+        this.shows = shows;
+    }
+
+    public double getDailyOther() {
+        return dailyOther;
+    }
+
+    public void setDailyOther(double dailyOther) {
+        this.dailyOther = dailyOther;
+    }
+
+    @Override
+    public String toString() {
+        return "Budget{" +
+                "home=" + home +
+                '}';
+    }
+
 
 
 }
