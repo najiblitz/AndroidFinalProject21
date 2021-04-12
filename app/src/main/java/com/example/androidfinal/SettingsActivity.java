1af9f98e4762e0377e1b109d534c93170ef6cfeb
Amanda Gayer <72535138+najiblitz@users.noreply.github.com>
2021-04-12 14:12:02 -0400

    Issue #15- Settings *add settings

--- app/src/main/java/com/example/androidfinal/SettingsActivity.java
+++ app/src/main/java/com/example/androidfinal/SettingsActivity.java
@@ -1,12 +1,10 @@
 package com.example.androidfinal;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.prefs.PreferencesFactory;
+import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -14,19 +12,22 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
-        getSupportFragmentManager()
-                .beginTransaction()
-//                .replace(R.id.settings, new SettingsFragment())
-                .commit();
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
     }
 
-    public static class SettingsFragment extends PreferenceFragment {
-        public void onCreatePreferences(String rootKey) {
-//            setPreferenceScreen(R.xml.root_preferences, rootKey);
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
         }
     }
 }
